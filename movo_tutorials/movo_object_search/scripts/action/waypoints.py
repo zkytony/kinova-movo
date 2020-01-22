1f9342b6a0b67d1b9bc4b0d1cd41db615f03bbb7
Yoonchang Sung <yooncs8@csail.mit.edu>
2020-01-21 22:29:14 -0500

    merged yoon's fix. but didn't help

--- movo_tutorials/movo_object_search/scripts/action/waypoints.py
+++ movo_tutorials/movo_object_search/scripts/action/waypoints.py
@@ -6,6 +6,7 @@ from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
 class WaypointApply(object):
     def __init__(self, position, orientation):
+        self.goal_cnt = 0
         # Get an action client
         self.client = actionlib.SimpleActionClient('movo_move_base', MoveBaseAction)
         self.client.wait_for_server()
@@ -24,5 +25,71 @@ class WaypointApply(object):
         self.waypoint_execute()
 
     def waypoint_execute(self):
-        self.client.send_goal(self.goal)
+        self.client.send_goal(self.goal, self.done_cb, self.active_cb, self.feedback_cb) 
         self.client.wait_for_result()
+        result = self.client.get_result()
+        if str(moveit_error_dict[result.error_code]) != "SUCCESS":
+                rospy.logerr("Failed to pick, not trying further")
+                return
+        rospy.loginfo("111")
+        # self.motion_stop()
+        # return True
+
+    def active_cb(self):
+        rospy.loginfo("Goal pose "+str(self.goal_cnt+1)+" is now being processed by the Action Server...")
+
+    def feedback_cb(self, feedback):
+        #To print current pose at each feedback:
+        #rospy.loginfo("Feedback for goal "+str(self.goal_cnt)+": "+str(feedback))
+        rospy.loginfo("Feedback for goal pose "+str(self.goal_cnt+1)+" received")
+
+    def done_cb(self, status, result):
+        self.goal_cnt += 1
+        # Reference for terminal status values: http://docs.ros.org/diamondback/api/actionlib_msgs/html/msg/GoalStatus.html
+        if status == 2:
+            rospy.loginfo("Goal pose "+str(self.goal_cnt)+" received a cancel request after it started executing, completed execution!")
+
+        if status == 3:
+            rospy.loginfo("Goal pose "+str(self.goal_cnt)+" reached") 
+            if self.goal_cnt< len(self.pose_seq):
+                next_goal = MoveBaseGoal()
+                next_goal.target_pose.header.frame_id = "map"
+                next_goal.target_pose.header.stamp = rospy.Time.now()
+                next_goal.target_pose.pose = self.pose_seq[self.goal_cnt]
+                rospy.loginfo("Sending goal pose "+str(self.goal_cnt+1)+" to Action Server")
+                rospy.loginfo(str(self.pose_seq[self.goal_cnt]))
+                self.client.send_goal(next_goal, self.done_cb, self.active_cb, self.feedback_cb) 
+            else:
+                rospy.loginfo("Final goal pose reached!")
+                rospy.signal_shutdown("Final goal pose reached!")
+                return
+
+        if status == 4:
+            rospy.loginfo("Goal pose "+str(self.goal_cnt)+" was aborted by the Action Server")
+            rospy.signal_shutdown("Goal pose "+str(self.goal_cnt)+" aborted, shutting down!")
+            return
+
+        if status == 5:
+            rospy.loginfo("Goal pose "+str(self.goal_cnt)+" has been rejected by the Action Server")
+            rospy.signal_shutdown("Goal pose "+str(self.goal_cnt)+" rejected, shutting down!")
+            return
+
+        if status == 8:
+            rospy.loginfo("Goal pose "+str(self.goal_cnt)+" received a cancel request before it started executing, successfully cancelled!")
+
+    def motion_stop(self, duration=1.0):
+        self._cfg_cmd.gp_cmd = 'GENERAL_PURPOSE_CMD_NONE'
+        self._cfg_cmd.gp_param = 0
+        self._cfg_cmd.header.stamp = rospy.get_rostime()
+        self._cfg_pub.publish(self._cfg_cmd)
+
+        rospy.logdebug("Stopping velocity command to movo base from BaseVelTest class ...")
+        try:
+            r = rospy.Rate(10)
+            start_time = rospy.get_time()
+            while (rospy.get_time() - start_time) < duration:
+                self._base_vel_pub.publish(Twist())
+                r.sleep()
+        except Exception as ex:
+            print "Message of base motion failed to be published, error message: ", ex.message
+            pass
