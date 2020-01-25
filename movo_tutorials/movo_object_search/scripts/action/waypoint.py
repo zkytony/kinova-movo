d952c91a619d9de750f3e8df0e40dde078bea61c
Kaiyu Zheng <kaiyutony@gmail.com>
2020-01-25 06:39:14 -0500

    waypoint fix?

--- movo_tutorials/movo_object_search/scripts/action/waypoint.py
+++ movo_tutorials/movo_object_search/scripts/action/waypoint.py
@@ -10,24 +10,27 @@ for name in MoveItErrorCodes.__dict__.keys():
 
 
 class WaypointApply(object):
-    """Note: adapted from code by Yoon;
-    Only applies one move base goal and waits for it to be reached."""
     class Status:
         NOT_RUNNING = "not_running"
         RUNNING = "running"
         SUCCESS = "success"
         FAIL = "fail"
-        
     def __init__(self, position, orientation, action_name="navigate"):
         # Get an action client
         self.client = actionlib.SimpleActionClient('movo_move_base', MoveBaseAction)
-        self.client.wait_for_server()
+        rospy.loginfo("Waiting for movo_move_base AS...")
+        if not self.client.wait_for_server(rospy.Duration(20)):
+            rospy.logerr("Could not connect to movo_move_base AS")
+            exit()
+        rospy.loginfo("Connected!")
+        rospy.sleep(1.0)
 
         self.status = WaypointApply.Status.NOT_RUNNING
         self.action_name = action_name
 
         # Define the goal
-        # print "type(position[0]):", type(position[0])
+        rospy.loginfo("Waypoint (%.2f,%.2f) and (%.2f,%.2f,%.2f,%.2f) is sent.", position[0], position[1], orientation[0], \
+            orientation[1], orientation[2], orientation[3])
         self.goal = MoveBaseGoal()
         self.goal.target_pose.header.frame_id = 'map'
         self.goal.target_pose.pose.position.x = position[0]
@@ -41,15 +44,9 @@ class WaypointApply(object):
 
     def waypoint_execute(self):
         self.status = WaypointApply.Status.RUNNING
-        self.client.send_goal(self.goal, self.done_cb, self.active_cb, self.feedback_cb) 
-        status = self.client.wait_for_result()
-        result = self.client.get_result()
-        if str(moveit_error_dict[result.error_code]) != "SUCCESS":
-            self.status = WaypointApply.Status.FAIL
-            rospy.logerr("Failed to pick, not trying further")
-            return
-        rospy.loginfo("Waypoint reached.")
-        self.status = WaypointApply.Status.SUCCESS
+        self.client.send_goal(self.goal, self.done_cb, self.active_cb, self.feedback_cb)
+        if self.client.wait_for_result():
+            rospy.loginfo("Goal is reached at (%.2f, %.2f).", self.goal.target_pose.pose.position.x, self.goal.target_pose.pose.position.y)
 
     def active_cb(self):
         rospy.loginfo("Navigation action "+str(self.action_name)+" is now being processed by the Action Server...")
@@ -63,30 +60,18 @@ class WaypointApply(object):
         # Reference for terminal status values: http://docs.ros.org/diamondback/api/actionlib_msgs/html/msg/GoalStatus.html
         if status == 2:
             rospy.loginfo("Navigation action "+str(self.action_name)+" received a cancel request after it started executing, completed execution!")
+            self.status = WaypointApply.Status.FAIL            
         elif status == 3:
             rospy.loginfo("Navigation action "+str(self.action_name)+" reached")
+            self.status = WaypointApply.Status.SUCCESS
         elif status == 4:
             rospy.loginfo("Navigation action "+str(self.action_name)+" was aborted by the Action Server")
             rospy.signal_shutdown("Navigation action "+str(self.action_name)+" aborted, shutting down!")
+            self.status = WaypointApply.Status.FAIL
         elif status == 5:
             rospy.loginfo("Navigation action "+str(self.action_name)+" has been rejected by the Action Server")
             rospy.signal_shutdown("Navigation action "+str(self.action_name)+" rejected, shutting down!")
+            self.status = WaypointApply.Status.FAIL
         elif status == 8:
             rospy.loginfo("Navigation action "+str(self.action_name)+" received a cancel request before it started executing, successfully cancelled!")
-
-    def motion_stop(self, duration=1.0):
-        self._cfg_cmd.gp_cmd = 'GENERAL_PURPOSE_CMD_NONE'
-        self._cfg_cmd.gp_param = 0
-        self._cfg_cmd.header.stamp = rospy.get_rostime()
-        self._cfg_pub.publish(self._cfg_cmd)
-
-        rospy.logdebug("Stopping velocity command to movo base from BaseVelTest class ...")
-        try:
-            r = rospy.Rate(10)
-            start_time = rospy.get_time()
-            while (rospy.get_time() - start_time) < duration:
-                self._base_vel_pub.publish(Twist())
-                r.sleep()
-        except Exception as ex:
-            print "Message of base motion failed to be published, error message: ", ex.message
-            pass
+            self.status = WaypointApply.Status.FAIL
